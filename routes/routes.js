fe4efdeda58c91cb69ee47fff5eb7a605713e249
tafseer-khan <tafseer.k.khan@gmail.com>
2021-01-31 18:24:40 -0600

    using express.static('public')

--- routes/routes.js
+++ routes/routes.js
@@ -5,38 +5,38 @@ module.exports = (app) => {
     fs.readFile("db/db.json", (err, data) => {
         if (err)
             throw error;
-        let note = JSON.parse(data);
+        let notes = JSON.parse(data);
 
         app.get('/notes', (req, res) => {
             res.sendFile(path.join(__dirname, '../public/notes.html'));
-        })
+        });
         app.get('*', (req, res) => {
             res.sendFile(path.join(__dirname, '../public/index.html'));
-        })
+        });
 
         app.get("/api/notes", (req, res) => {
-            res.json(note);
-        })
+            res.json(notes);
+        });
 
         app.post("/api/notes", (req, res) => {
             let newNote = req.body;
-            note.push(newNote);
+            notes.push(newNote);
             updateDB();
-            console.log("Added new note")
-        })
+            console.log("Added new notes")
+        });
 
         app.get("/api/notes/:id", (req, res) => {
-            res.json(note[req.params.id]);
-        })
+            res.json(notes[req.params.id]);
+        });
 
         app.delete("/api/notes/:id", (req, res) => {
-            note.splice(req.params.id, 1);
+            notes.splice(req.params.id, 1);
             updateDB()
-            console.log("Deleted note")
-        })
+            console.log("Deleted notes")
+        });
 
         updateDB = () => {
-            fs.writeFile("db/db.json", JSON.stringify(note), err => {
+            fs.writeFile("db/db.json", JSON.stringify(notes), err => {
                 if (err) throw err;
                 return true;
             })
--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const fs = require("fs");
+const path = require("path")
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
+app.use(express.static("public"));
 
 require('./routes/routes')(app);
 
